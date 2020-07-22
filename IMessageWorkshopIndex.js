8f77e9271e5ebb348e35999de81edc8800f4fa43
Jimmy Gunter <gunterj@amazon.com>
2020-07-22 09:33:00 -0700

    add js/css files
    add initial js and css files

---
+++ IMessageWorkshopIndex.js
@@ -0,0 +1,27 @@
+(function(){
+    var comfortable = ["Accepted","Acknowledged","Amused","Appreciated","Attracted","Attractive","Calm","Capable","Caring","Competent","Confident","Connected","Considered","Content","Creative","Curious","Delighted","Empowered","Encouraged","Enthusiastic","Excited","Exhilarated","Grateful","Happy","Hopeful","Important","Included","Independent","Inspired","Interested","Liberated","Loved","Nurtured","Passionate","Protected","Proud","Reassured","Relaxed","Relieved","Respected","Safe","Satisfied","Secure","Stimulated","Supported","Surprised","Trusted","Trusting","Understood","Valued","Welcome"];
+    var uncomfortable = ["Abandoned","Afraid","Anxious","Belittled","Betrayed","Concerned","Confused","Controlled","Deceived","Defeated","Defensive","Devastated","Disappointed","Disconnected","Discounted","Discouraged","Disrespected","Embarrassed","Excluded","Foolish","Frustrated","Grief","Guilty","Humiliated","Inadequate","Inferior","Insecure","Jealous","Lonely","Manipulated","Nervous","Obligated","Offended","Overwhelmed","Panic","Powerless","Pressured","Regret","Rejected","Resentful","Sad","Shame","Shocked","Surprised","Trapped","Unappreciated","Unattractive","Violated","Vulnerable","Worried"];
+    var both = comfortable.concat(uncomfortable);
+
+    var updateMessage = function(emotionList){
+        var emotion = emotionList[Math.floor(Math.random() * emotionList.length)];
+
+        var output = "I feel " + emotion + " when _________________ because ___________________";
+        $("imessage").html(output);
+    }
+
+    $("#imessage-form").submit(function(event){
+        var comfortable = $("checkbox-comfortable").prop("checked");
+        var uncomfortable = $("checkbox-uncomfortable").prop("checked");
+
+        if(comfortable && uncomfortable){
+            updateMessage(both);
+        } else if(comfortable){
+            updateMessage(comfortable);
+        } else if(uncomfortable){
+            updateMessage(uncomfortable);
+        } else {
+            updateMessage(both);
+        }
+    });
+})();
