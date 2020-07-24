b4a1a2955f2e83c8e9cfc013961f4078fe721f74
Jimmy Gunter <gunterj@amazon.com>
2020-07-24 10:12:25 -0700

    fix emotion list retrieval

--- IMessageWorkshopIndex.js
+++ IMessageWorkshopIndex.js
@@ -1,7 +1,7 @@
 (function () {
-    var comfortable = ["Accepted","Acknowledged","Amused","Appreciated","Attracted","Attractive","Calm","Capable","Caring","Competent","Confident","Connected","Considered","Content","Creative","Curious","Delighted","Empowered","Encouraged","Enthusiastic","Excited","Exhilarated","Grateful","Happy","Hopeful","Important","Included","Independent","Inspired","Interested","Liberated","Loved","Nurtured","Passionate","Protected","Proud","Reassured","Relaxed","Relieved","Respected","Safe","Satisfied","Secure","Stimulated","Supported","Surprised","Trusted","Trusting","Understood","Valued","Welcome"];
-    var uncomfortable = ["Abandoned","Afraid","Anxious","Belittled","Betrayed","Concerned","Confused","Controlled","Deceived","Defeated","Defensive","Devastated","Disappointed","Disconnected","Discounted","Discouraged","Disrespected","Embarrassed","Excluded","Foolish","Frustrated","Grief","Guilty","Humiliated","Inadequate","Inferior","Insecure","Jealous","Lonely","Manipulated","Nervous","Obligated","Offended","Overwhelmed","Panic","Powerless","Pressured","Regret","Rejected","Resentful","Sad","Shame","Shocked","Surprised","Trapped","Unappreciated","Unattractive","Violated","Vulnerable","Worried"];
-    var both = comfortable.concat(uncomfortable);
+    var COMFORTABLE_EMOTION_LIST = ["Accepted","Acknowledged","Amused","Appreciated","Attracted","Attractive","Calm","Capable","Caring","Competent","Confident","Connected","Considered","Content","Creative","Curious","Delighted","Empowered","Encouraged","Enthusiastic","Excited","Exhilarated","Grateful","Happy","Hopeful","Important","Included","Independent","Inspired","Interested","Liberated","Loved","Nurtured","Passionate","Protected","Proud","Reassured","Relaxed","Relieved","Respected","Safe","Satisfied","Secure","Stimulated","Supported","Surprised","Trusted","Trusting","Understood","Valued","Welcome"];
+    var UNCOMFORTABLE_EMOTION_LIST = ["Abandoned","Afraid","Anxious","Belittled","Betrayed","Concerned","Confused","Controlled","Deceived","Defeated","Defensive","Devastated","Disappointed","Disconnected","Discounted","Discouraged","Disrespected","Embarrassed","Excluded","Foolish","Frustrated","Grief","Guilty","Humiliated","Inadequate","Inferior","Insecure","Jealous","Lonely","Manipulated","Nervous","Obligated","Offended","Overwhelmed","Panic","Powerless","Pressured","Regret","Rejected","Resentful","Sad","Shame","Shocked","Surprised","Trapped","Unappreciated","Unattractive","Violated","Vulnerable","Worried"];
+    var COMBINED_EMOTION_LIST = comfortable.concat(uncomfortable);
 
     var updateMessage = function(emotionList){
         var emotion = emotionList[Math.floor(Math.random() * emotionList.length)];
@@ -19,13 +19,13 @@
             var uncomfortable = $("#btn-uncomfortable").hasClass("active");
 
             if(comfortable && uncomfortable){
-                updateMessage(both);
-            } else if(comfortable){
+                updateMessage(COMBINED_EMOTION_LIST);
+            } else if(COMFORTABLE_EMOTION_LIST){
                 updateMessage(comfortable);
-            } else if(uncomfortable){
+            } else if(UNCOMFORTABLE_EMOTION_LIST){
                 updateMessage(uncomfortable);
             } else {
-                updateMessage(both);
+                updateMessage(COMBINED_EMOTION_LIST);
             }
         });
     };
