f632651fb366680ba2bc6596d78772b2f09fcfd8
Jimmy Gunter <gunterj@amazon.com>
2020-07-24 09:19:02 -0700

    Update IMessageWorkshopIndex.js
    update iife and form handler

--- IMessageWorkshopIndex.js
+++ IMessageWorkshopIndex.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     var comfortable = ["Accepted","Acknowledged","Amused","Appreciated","Attracted","Attractive","Calm","Capable","Caring","Competent","Confident","Connected","Considered","Content","Creative","Curious","Delighted","Empowered","Encouraged","Enthusiastic","Excited","Exhilarated","Grateful","Happy","Hopeful","Important","Included","Independent","Inspired","Interested","Liberated","Loved","Nurtured","Passionate","Protected","Proud","Reassured","Relaxed","Relieved","Respected","Safe","Satisfied","Secure","Stimulated","Supported","Surprised","Trusted","Trusting","Understood","Valued","Welcome"];
     var uncomfortable = ["Abandoned","Afraid","Anxious","Belittled","Betrayed","Concerned","Confused","Controlled","Deceived","Defeated","Defensive","Devastated","Disappointed","Disconnected","Discounted","Discouraged","Disrespected","Embarrassed","Excluded","Foolish","Frustrated","Grief","Guilty","Humiliated","Inadequate","Inferior","Insecure","Jealous","Lonely","Manipulated","Nervous","Obligated","Offended","Overwhelmed","Panic","Powerless","Pressured","Regret","Rejected","Resentful","Sad","Shame","Shocked","Surprised","Trapped","Unappreciated","Unattractive","Violated","Vulnerable","Worried"];
     var both = comfortable.concat(uncomfortable);
@@ -10,8 +10,9 @@
         $("imessage").html(output);
     }
 
-    $("#button-getmessage").click(function(event){
-        event.preventDefault();
+    $("#imessage-form").submit(function(event){ 
+        event.preventDefault(); 
+        console.log(event);
 
         var comfortable = $("checkbox-comfortable").prop("checked");
         var uncomfortable = $("checkbox-uncomfortable").prop("checked");
@@ -26,4 +27,4 @@
             updateMessage(both);
         }
     });
-})();
+}());
